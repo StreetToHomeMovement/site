980939b56bb343461a468780417f6ec75237fb85
Darren Deanto <darrengdeanto@gmail.com>
2016-08-20 14:39:51 -0400

    braintree nonce saving to db, basic flow of site beta 3

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ var bodyParser = require('body-parser')
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 var Parse = require('parse/node')
 var cookieParser = require('cookie-parser')
+var braintree = require('braintree');
+var passwords = require('./passwords.json')
+var gateway = braintree.connect({
+	environment:  braintree.Environment.Sandbox,
+	merchantId:   passwords.braintree.merchantId,
+	publicKey:    passwords.braintree.publicKey,
+	privateKey:   passwords.braintree.privateKey
+});
 
 Parse.initialize("sukeiran44ka88aj");
 Parse.serverURL = 'https://parse-server-codecraft-x-ample.herokuapp.com/parse';
@@ -16,36 +24,58 @@ app.use('/public', express.static(__dirname + '/public'));
 // use ejs view engine
 app.set('view engine', 'ejs')
 
+
 app.get('/', function(req, res) {
   res.render(__dirname + '/public/views/index.ejs', {firstname: req.cookies.firstname})
 })
 
-app.post('/pay', urlencodedParser, function(req, res) {
+
+app.post('/pay', urlencodedParser, function (request, response) {
+
   // set cookies
   var dateIn30Days = new Date(Date.now() + 1000*60*60*24*30)
-  res.cookie('email', req.body.email, { expires: dateIn30Days })
-  res.cookie('zip', req.body.zip, { expires: dateIn30Days })
+  response.cookie('email', request.body.email, { expires: dateIn30Days })
+  response.cookie('zip', request.body.zip, { expires: dateIn30Days })
 
   // save to database
   var Joined = new Parse.Object.extend("joined")
   var joined = new Joined()
 
-	joined.set("email",req.body.email)
-	joined.set("zip",req.body.zip)
+	joined.set("email",request.body.email)
+	joined.set("zip",request.body.zip)
 
 	joined.save().then( function success(obj) {
 		  console.log("client joined with id " + obj.id)
-      res.sendFile(__dirname + '/public/views/pay.html')
+      gateway.clientToken.generate({}, function (err, res) {
+        response.render(__dirname + '/public/views/pay.ejs', {
+          clientToken: res.clientToken
+        });
+      });
 		}, function error(err) {
 			console.error(err)
 		})
 
-})
+
+
+});
 
 // see this regarding users logging in without re-entering pwd:
 // http://stackoverflow.com/questions/5009685/encoding-cookies-so-they-cannot-be-spoofed-or-read-etc/5009903#5009903
-app.post('/checkout', function(req, res) {
-  res.sendFile(__dirname + '/public/views/setpassword.html')
+app.post('/setaccount', urlencodedParser, function(req, res) {
+  // save to database
+  var Donation = new Parse.Object.extend("Donation")
+  var donation = new Donation()
+
+	donation.set("payment_method_nonce",req.body.payment_method_nonce)
+	donation.set("amount",req.body.amount)
+  //donation.set("")
+
+	donation.save().then( function success(obj) {
+		  console.log("donation made with id " + obj.id)
+      res.sendFile(__dirname + '/public/views/setaccount.html')
+		}, function error(err) {
+			console.error(err)
+		})
 })
 
 app.get('/user', function(req, res) {
@@ -56,7 +86,7 @@ app.get('/user', function(req, res) {
     })
   }
   else {
-    res.sendFile(__dirname + '/public/views/setpassword.html')
+    res.sendFile(__dirname + '/public/views/setaccount.html')
   }
 })
 
@@ -97,6 +127,14 @@ app.post('/user', urlencodedParser, function(req, res) {
 			console.error(err)
 		})
 
+})
+
+app.get('/login', function(req, res) {
+  res.sendFile(__dirname + '/public/views/login.html')
+})
+
+app.get('/resetpwd', function(req, res) {
+  res.sendFile(__dirname + '/public/views/resetpwd.html')
 })
 
 app.listen(port)
