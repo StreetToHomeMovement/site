a94d6b091cb73170df8fe761e283ab87d75414c9
Darren Deanto <darrengdeanto@gmail.com>
2016-08-24 23:27:52 -0400

    cleaned things up but had to implement javascript hack on /pay page

--- app.js
+++ app.js
@@ -10,14 +10,15 @@ var payController = require('./controllers/pay.js')
 var resetpwdController = require('./controllers/resetpwd.js')
 var setaccountController = require('./controllers/setaccount.js')
 var userController = require('./controllers/user.js')
-
-app.use(cookieParser())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use('/public', express.static(__dirname + '/public'));
+var errorController = require('./controllers/error.js')
 
 // use ejs view engine
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views');
+
+app.use(cookieParser())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use('/public', express.static(__dirname + '/public'));
 
 // set controllers (routes)
 indexController(app)
@@ -26,5 +27,6 @@ payController(app)
 resetpwdController(app)
 setaccountController(app)
 userController(app)
+errorController(app)
 
 app.listen(port)
