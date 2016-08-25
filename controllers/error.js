c6caa7f39a651a99ae77d972a63458d43aef7805
Darren Deanto <darrengdeanto@gmail.com>
2016-08-25 00:16:52 -0400

    basic error handler

---
+++ controllers/error.js
@@ -0,0 +1,19 @@
+module.exports = function(app) {
+
+  app.get('*', function(req, res, next) {
+    var err = new Error()
+    err.status = 404
+    next(err)
+  })
+
+  // handling 404 errors
+  app.use(function(err, req, res, next) {
+    if(err.status !== 404) {
+      return next()
+    }
+
+    res.send(err.message || '** not a valid route **')
+  })
+
+
+ }
