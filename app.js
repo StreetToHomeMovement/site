aaf8969f8cd4b882f72b8de8caca08647aa906d4
Darren Deanto <darrengdeanto@gmail.com>
2016-08-29 01:48:03 -0400

    don't need reset pwd code because mailgun takes care of it

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ var cookieParser = require('cookie-parser')
 var indexController = require('./controllers/index.js')
 var loginController = require('./controllers/login.js')
 var payController = require('./controllers/pay.js')
-var resetpwdController = require('./controllers/resetpwd.js')
 var setaccountController = require('./controllers/setaccount.js')
 var userController = require('./controllers/user.js')
 var errorController = require('./controllers/error.js')
@@ -20,6 +19,27 @@ var api = new ParseServer({
 
 	appId: process.env.APP_ID || 'sukeiran44ka88aj',
 	masterKey: process.env.MASTER_KEY || '33jlas9893jkla', //Add your master key here. Keep it secret!
+
+	//**** Email Verification ****//
+	/* Enable email verification */
+	verifyUserEmails: true,
+	/* The public URL of your app */
+	// This will appear in the link that is used to verify email addresses and reset passwords.
+	/* Set the mount path as it is in serverURL */
+	publicServerURL: process.env.SERVER_URL || 'http://localhost:3000/parse',
+	/* This will appear in the subject and body of the emails that are sent */
+	appName: process.env.APP_NAME || "StreetToFightMovement",
+
+	 emailAdapter: {
+	 	module: 'parse-server-simple-mailgun-adapter',
+	 	options: {
+	 		fromAddress: process.env.EMAIL_FROM || "no-reply@getsidewalk.com",
+	 		domain: process.env.MAILGUN_DOMAIN || "appe8b1d4d285fe49edafea07f3c1477cb3.mailgun.org",
+			apiKey: process.env.MAILGUN_API_KEY  || "key-e0f292602a17f7ae7214409c83baba81"
+		}
+	},
+
+
 });
 
 var x = process.env.SERVER_URL || 'http://localhost:3000/parse'
@@ -41,7 +61,6 @@ app.use('/public', express.static(__dirname + '/public'));
 indexController(app)
 loginController(app)
 payController(app)
-resetpwdController(app)
 setaccountController(app)
 userController(app)
 errorController(app)
--- controllers/resetpwd.js
+++
@@ -1,9 +0,0 @@
-var path = require('path')
-
-module.exports = function(app) {
-
-  app.get('/resetpwd', function(req, res) {
-    res.sendFile(path.join(__dirname, '..', '/views/resetpwd.html'))
-  })
-
-}
