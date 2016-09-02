f93b8348a2d293e0937119a99f5b5b927f78655f
Darren Deanto <darrengdeanto@gmail.com>
2016-09-02 18:13:21 -0400

    can retrieve basic info from database

--- cloud/main.js
+++ cloud/main.js
@@ -1,38 +1,3 @@
-Parse.Cloud.define('linkJoinedWithUser', function(req, res) {
-
-  Parse.Cloud.useMasterKey()
-
-  // unmangle
-  var token =  req.user.getSessionToken()
-  var user = req.user
-  var joined_id = req.params.joined_id
-  console.log("sessionToken: " + token) //+ " and joined id is " + joined_id)
-  console.log("user id is: " + user.id) //+ " and joined id is " + joined_id)
-  console.log("joined_id is: " + joined_id)
-
-  // get joined obj
-  q = new Parse.Query("joined")
-	q.equalTo("objectId", joined_id)
-	q.first().then(function(joined) {
-    console.log("found " + joined.id)
-
-    // update user obj to reference joined obj
-    user.set("joined", joined)
-    // save to db
-
-    console.log('attempting to save')
-    user.save().then( function success(obj) {
-        console.log("user updated joined with joined id: " + joined.id)
-      }, function error(err) {
-        console.error(err)
-      })
-
-  }, function(err) {
-    console.error(err)
-  })
-
-})
-
 Parse.Cloud.define('totalDonations', function(req, res) {
   // unmangle
   var token =  req.user.getSessionToken()
