f30857aaa0f296b66dcf9c9d6898c68505990f3d
Darren Deanto <darrengdeanto@gmail.com>
2016-09-05 13:40:41 -0400

    fixed query method

--- cloud/main.js
+++ cloud/main.js
@@ -1,3 +1,38 @@
+Parse.Cloud.define('linkJoinedWithUser', function(req, res) {
+
+  Parse.Cloud.useMasterKey()
+
+  // unmangle
+  var token =  req.user.getSessionToken()
+  var user = req.user
+  var joined_id = req.params.joined_id
+  console.log("sessionToken: " + token) //+ " and joined id is " + joined_id)
+  console.log("user id is: " + user.id) //+ " and joined id is " + joined_id)
+  console.log("joined_id is: " + joined_id)
+
+  // get joined obj
+  q = new Parse.Query("joined")
+	q.equalTo("objectId", joined_id)
+	q.first().then(function(joined) {
+    console.log("found " + joined.id)
+
+    // update user obj to reference joined obj
+    user.set("joined", joined)
+    // save to db
+
+    console.log('attempting to save')
+    user.save().then( function success(obj) {
+        console.log("user updated joined with joined id: " + joined.id)
+      }, function error(err) {
+        console.error(err)
+      })
+
+  }, function(err) {
+    console.error(err)
+  })
+
+})
+
 Parse.Cloud.define('totalDonations', function(req, res) {
   // unmangle
   var token =  req.user.getSessionToken()
@@ -31,7 +66,7 @@ Parse.Cloud.define('personalDonations', function(req, res) {
   console.log(joined.id)
 
   var jq = new Parse.Query("joined")
-  jq.find('objectId', joined.id)
+  jq.equalTo('objectId', joined.id)
 
   var dq = new Parse.Query("Donation")
   dq.matchesQuery('joined', jq)
