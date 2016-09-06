8e237f40e650b2c4812348e067a3e5b8d1ecd472
Darren Deanto <darrengdeanto@gmail.com>
2016-09-05 23:47:56 -0400

    user page has nav panel

--- controllers/setaccount.js
+++ controllers/setaccount.js
@@ -1,6 +1,6 @@
 var path = require('path')
 var Parse = require('../helpers/parse_server')
-// try to refactor by chaining "then" twice 
+// try to refactor by chaining "then" twice
 
 module.exports = function(app) {
 
@@ -25,6 +25,21 @@ module.exports = function(app) {
         })
     }, function(err) {
       console.log(err)
+    });
+
+    // create customer in braintree db
+    var gateway = require('../helpers/braintree_gateway.js').gateway
+    gateway.transaction.sale({
+      amount: req.body.amount,
+      paymentMethodNonce: req.body.payment_method_nonce,
+      customer: {
+        id: joined_id
+      },
+      options: {
+        storeInVaultOnSuccess: true
+      }
+    }, function (err, result) {
+      console.log("success?")
     });
 
   	res.sendFile(path.join(__dirname, '..', '/views/setaccount.html'))
