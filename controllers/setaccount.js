63d1aab01a3074507bbfd78cd4e37de80208fe38
Darren Deanto <darrengdeanto@gmail.com>
2016-09-08 06:07:09 -0400

    slimmed down

--- controllers/setaccount.js
+++ controllers/setaccount.js
@@ -10,54 +10,63 @@ module.exports = function(app) {
     var email = req.cookies.email
     var payment_method_nonce = req.body.payment_method_nonce
     var amount = req.body.amount
+    var subscription = req.body.subscription
 
-    // set up query
-    q = new Parse.Query('BraintreeMap')
-    q.equalTo('email',req.cookies.email)
+    if (subscription) {
 
-    // wuery
-    q.first().then(function(existingMap) {
-      // if no existing braintreeCustomer create customer in braintree dashboard
-      // and add hash to braintreeMap table in our Parse db
-      if (existingMap === undefined) {
-        gateway.transaction.sale({
-          amount: amount,
-          paymentMethodNonce: payment_method_nonce,
-          customer: {
-            email: email
-          },
-          options: {
-            storeInVaultOnSuccess: true
-          }
-        }, function (err, result) {
-          console.log(result.transaction.customer.id)
+      gateway.customer.create({
+        email: email,
+        paymentMethodNonce: payment_method_nonce
+      }, function (err,result) {
+        console.error(err)
+        console.log(result)
+        // now that we created a customer, lets make subscription
+        gateway.subscription.create({
+          planId: "monthly10",
+          paymentMethodToken: result.customer.paymentMethods[0].token
+        }, function (err,result) {
+          console.error(err)
+          console.log(result)
+          // now that we made a subscription, lets store the
+          // email - braintree id hash in our db
           var BraintreeMap = new Parse.Object.extend("BraintreeMap")
           var braintreeMap = new BraintreeMap()
           braintreeMap.set('email',email)
-          braintreeMap.set('braintreeCustomerId',result.transaction.customer.id)
           braintreeMap.save().then( function success(obj) {
             console.log("braintree hash made with id " + obj.id)
           }, function error(err) {
             console.error(err)
           })
         })
-      } else { // add transaction to existing customer
-        console.log(existingMap.get('braintreeCustomerId'))
-        gateway.transaction.sale({
-          amount: amount,
-          paymentMethodNonce: payment_method_nonce,
-          customerId: existingMap.get('braintreeCustomerId')
-        }, function (err, result) {
-          if (err) {
-            console.error(err)
-          } else {
-            console.log(result)
-          }
-        });
-      }
-    }, function(err) {
-      console.error(err)
-    })
+      })
+
+    } else {
+
+      gateway.transaction.sale({
+        amount: amount,
+        paymentMethodNonce: payment_method_nonce,
+        customer: {
+          email: email
+        },
+        options: {
+          storeInVaultOnSuccess: true
+        }
+      }, function (err,result) {
+        console.error(err)
+        console.log(result)
+        // now that we made a transaction, lets store the
+        // email - braintree id hash in our db
+        var BraintreeMap = new Parse.Object.extend("BraintreeMap")
+        var braintreeMap = new BraintreeMap()
+        braintreeMap.set('email',email)
+        braintreeMap.save().then( function success(obj) {
+          console.log("braintree hash made with id " + obj.id)
+        }, function error(err) {
+          console.error(err)
+        })
+      })
+
+    }
 
   	res.sendFile(path.join(__dirname, '..', '/views/setaccount.html'))
   })
