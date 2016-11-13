dc2d7889cc25b172d348528994aaac672fe5fa2e
Darren Deanto <darrengdeanto@gmail.com>
2016-11-13 01:44:57 -0500

    payment is broken up into two views

--- controllers/paymentMethod.js
+++ controllers/paymentMethod.js
@@ -2,11 +2,15 @@ var gateway = require('../helpers/braintree_gateway.js').gateway
 
 module.exports = function(app) {
 
-  app.get('/paymentMethod', function (req, res) {
-
+  app.get('/paymentMethod/:amount/:subscription', function (req, res) {
+    
   	// generate braintree client token and render html
   	gateway.clientToken.generate({}, function (err, res2) {
-  	  res.render('paymentMethod.ejs', { clientToken: res2.clientToken })
+  	  res.render('paymentMethod.ejs', {
+        clientToken: res2.clientToken,
+        amount: req.params.amount,
+        subscription: req.params.subscription
+      })
   	})
 
   })
--- public/js/braintree_setup.js
+++ public/js/braintree_setup.js
@@ -1,26 +1,28 @@
-function braintreeSetup(returningUser) {
+function braintreeSetup(returningUser, amount, subscription) {
   Parse.Cloud.run("braintreeClientToken").then(function(clientToken) {
     braintree.setup(clientToken, 'dropin', {
         container: 'checkout',
         onPaymentMethodReceived: function(result) {
 
-          var radios = document.getElementsByName('amount');
-          var amount
-          for (var i = 0, length = radios.length; i < length; i++) {
-              if (radios[i].checked) {
-                  // do whatever you want with the checked radio
-                  amount = radios[i].value
-                  // only one radio can be logically checked, don't check the rest
-                  break;
-              }
-          }
+          alert(amount)
 
-          var subscription
-          if (document.getElementById('subscription').checked) {
-            subscription = true
-          } else {
-            subscription = false
-          }
+          // var radios = document.getElementsByName('amount');
+          // var amount
+          // for (var i = 0, length = radios.length; i < length; i++) {
+          //     if (radios[i].checked) {
+          //         // do whatever you want with the checked radio
+          //         amount = radios[i].value
+          //         // only one radio can be logically checked, don't check the rest
+          //         break;
+          //     }
+          // }
+          //
+          // var subscription
+          // if (document.getElementById('subscription').checked) {
+          //   subscription = true
+          // } else {
+          //   subscription = false
+          // }
 
           if (returningUser) {
             var func1 = "createSubscription"
