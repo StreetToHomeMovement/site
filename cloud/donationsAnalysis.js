726f06dcb4bea3ccc39db15313fe3986bef37dc5
Darren Deanto <darrengdeanto@gmail.com>
2016-11-27 16:06:39 -0500

    members computed on backend

--- cloud/donationsAnalysis.js
+++ cloud/donationsAnalysis.js
@@ -1,27 +1,51 @@
 var gateway = require('../helpers/braintree_gateway.js').gateway
 
 Parse.Cloud.define('memberDonations', function(req, res) {
+
   Parse.Cloud.useMasterKey()
 
   q = new Parse.Query('User')
   // q.select(['firstname','totalDonations'])
   q.exists('totalDonations')
   q.find().then(function(users) {
-    var output = []
+
+
+  memberLevels = {
+          'Diamond': [],
+        	'Ruby': [],
+         	'Jade': [],
+        	'Sapphire': [],
+        	'Emerald': [],
+        	'Opal': [],
+        	'Pearl': [],
+        	'Garnet': [],
+        	'Amethyst': [],
+        	'Quartz': [],
+        	'Zircon': []
+    }
+
+
+
+
+
+    console.log('HIHIH')
     for (i = 0; i < users.length; i++) {
+
       var user = users[i]
-      var j = user.get('firstname')
-      var firstname = user.get('firstname')
-      var totalDonations = user.get('totalDonations')
-      console.log(firstname)
-      output.push({
-        j: j,
-        firstname: firstname || 'Anonymous',
-        totalDonations: totalDonations
+      var donorName = user.get('firstname')
+      console.log(donorName)
+      var memberLevel = "Diamond"
+
+      console.log(memberLevel)
+
+      memberLevels.Diamond.push({
+        donorName: donorName || 'Anonymous',
+        memberLevel: memberLevel
       })
-      if (output.length === users.length) {
+
+      if (memberLevels.length === users.length) {
         console.log('done')
-        res.success(output)
+        res.success(memberLevels)
       }
     }
   })
--- controllers/members.js
+++ controllers/members.js
@@ -1,12 +1,79 @@
-var memberLevels = require('../test_data/memberLevels.js')
-
 module.exports = function(app) {
 
   app.get('/members', function(req, res) {
-    console.log(memberLevels)
-    console.log(memberLevels.length)
-    console.log()
-    res.render('members.ejs', {memberLevels: memberLevels})
+
+    var memberLevels = {
+            'Diamond': [],
+          	'Ruby': [],
+           	'Jade': [],
+          	'Sapphire': [],
+          	'Emerald': [],
+          	'Opal': [],
+          	'Pearl': [],
+          	'Garnet': [],
+          	'Amethyst': [],
+          	'Quartz': [],
+          	'Zircon': []
+      }
+
+    q = new Parse.Query('User')
+    q.exists('totalDonations')
+
+    q.find().then(function(users) {
+      for (i = 0; i < users.length; i++) {
+        var user = users[i]
+        var memberLevel = calculateMemberLevel(user.get('totalDonations'))
+        memberLevels[memberLevel].push(user.get('donorname') || user.get('firstname'))
+
+        if (i+1 === users.length) {
+          console.log(memberLevels)
+          res.render('members.ejs', {memberLevels: memberLevels})
+        }
+
+      }
+    })
+
   })
+}
+
+function calculateMemberLevel(totalDonations) {
+  var d = totalDonations
+
+  if (d >= 1000000) {
+    return 'Diamond'
+  } else if (d >= 500000) {
+    return 'Ruby'
+  } else if (d >= 250000) {
+    return 'Jade'
+  } else if (d >= 100000) {
+    return 'Sapphire'
+  } else if (d >= 25000) {
+    return 'Emerald'
+  } else if (d >= 10000) {
+    return 'Opal'
+  } else if (d >= 5000) {
+    return 'Pearl'
+  } else if (d >= 2500) {
+    return 'Garnet'
+  } else if (d >= 1000) {
+    return 'Amethyst'
+  } else if (d >= 500) {
+    return 'Quartz'
+  } else {
+    return 'Zircon'
+  }
 
 }
+
+// var memberLevels = require('../test_data/memberLevels.js')
+//
+// module.exports = function(app) {
+//
+//   app.get('/members', function(req, res) {
+//     console.log(memberLevels)
+//     console.log(memberLevels.length)
+//     console.log()
+//     res.render('members.ejs', {memberLevels: memberLevels})
+//   })
+//
+// }
