96201b337d7575222e6d9125cbb513338b0cd5c7
Darren Deanto <darrengdeanto@gmail.com>
2016-11-27 00:09:42 -0500

    hacked member page css

--- controllers/members.js
+++ controllers/members.js
@@ -1,9 +1,12 @@
+var memberLevels = require('../test_data/memberLevels.js')
+
 module.exports = function(app) {
 
   app.get('/members', function(req, res) {
-    Parse.Cloud.run("memberDonations").then(function(members) {
-      res.render('members.ejs', {members: members})
-    })
+    console.log(memberLevels)
+    console.log(memberLevels.length)
+    console.log()
+    res.render('members.ejs', {memberLevels: memberLevels})
   })
 
 }
---
+++ test_data/memberLevels.js
@@ -0,0 +1,15 @@
+memberLevels = {
+      'Diamond': ["Darren","Kerri","Sam"],
+    	'Ruby': ["Euceny","Donald Family"],
+     	'Jade': [],
+    	'Sapphire': ["Sam"],
+    	'Emerald': ["Deanto","Shibu Inu","Tarrantino","Terry Tao","O'neil"],
+    	'Opal': [],
+    	'Pearl': [],
+    	'Garnet': [],
+    	'Amethyst': [],
+    	'Quartz': ["Steve Fleg", "Daniel Slattery Synk"],
+    	'Zircon': []
+}
+
+module.exports = memberLevels
